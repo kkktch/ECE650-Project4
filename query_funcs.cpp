ae4ad10c9f6fb7e70b04d02bee8b52838aad0ce9
kaidi <kl347@duke.edu>
2020-03-09 14:03:02 -0400

    test

--- query_funcs.cpp
+++ query_funcs.cpp
@@ -4,9 +4,9 @@ void add_player(connection *C, int team_id, int jersey_num, string first_name, s
                 int mpg, int ppg, int rpg, int apg, double spg, double bpg)
 {
     work W(*C);
-    stringstream SQL = "INSERT INTO player (team_id, uniform_num, first_name, last_name, mpg, ppg, rpg, apg, spg, bpg) "
-                       "VALUES ( "
-                       << team_id << ", " << jersey_num << ", " << first_name << ", " << last_name << ", " << mpg << ", " << ppg << ", " << rpg << ", " << apg << ", " << spg << ", " << bpg << ");";
+    stringstream SQL;
+    SQL << "INSERT INTO player (team_id, uniform_num, first_name, last_name, mpg, ppg, rpg, apg, spg, bpg) VALUES ( "
+        << team_id << ", " << jersey_num << ", " << W.quote(first_name) << ", " << W.quote(last_name) << ", " << mpg << ", " << ppg << ", " << rpg << ", " << apg << ", " << spg << ", " << bpg << ");";
     cout << SQL.str();
     W.exec(SQL.str());
     W.commit();
