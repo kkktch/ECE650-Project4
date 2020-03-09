2fe5a8c2e2b7aa3fd0dde1f4941ef237f0821b80
kaidi <kl347@duke.edu>
2020-03-09 14:59:30 -0400

    TEST

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ using namespace pqxx;
 string CreatePlayer()
 {
   std::string ans = "CREATE TABLE PLAYER (​"
-                    "PLAYER_ID   INT PRIMARY KEY  NOT NULL,"
+                    "PLAYER_ID   SERIAL PRIMARY KEY  NOT NULL,"
                     "TEAM_ID     INT,"
                     "UNIFORM_NUM INT,"
                     "FIRST_NAME  VARCHAR(256),"
@@ -29,7 +29,7 @@ string CreatePlayer()
 string CreateTeam()
 {
   std::string ans = "CREATE TABLE TEAM (​"
-                    "TEAM_ID   INT PRIMARY KEY  NOT NULL,"
+                    "TEAM_ID   SERIAL PRIMARY KEY  NOT NULL,"
                     "NAME      VARCHAR(256),"
                     "STATE_ID  INT,"
                     "COLOR_ID  INT,"
@@ -42,13 +42,13 @@ string CreateTeam()
 
 string CreateState()
 {
-  std::string ans = "CREATE TABLE STATE (​STATE_ID INT PRIMARY KEY NOT NULL, NAME VARCHAR(256));";
+  std::string ans = "CREATE TABLE STATE (​STATE_ID SERIAL PRIMARY KEY NOT NULL, NAME VARCHAR(256));";
   return ans;
 }
 
 string CreateColor()
 {
-  std::string ans = "CREATE TABLE COLOR (COLOR_ID INT PRIMARY KEY NOT NULL, NAME VARCHAR(256));";
+  std::string ans = "CREATE TABLE COLOR (COLOR_ID SERIAL PRIMARY KEY NOT NULL, NAME VARCHAR(256));";
   return ans;
 }
 
