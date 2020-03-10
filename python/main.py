3772c7f87b0ae7c51f197c98904ee2314da004b9
kaidi <kl347@duke.edu>
2020-03-10 17:21:20 -0400

    test

--- python/main.py
+++ python/main.py
@@ -2,7 +2,6 @@ from sqlalchemy import create_engine, Column, Integer, String, Sequence, select
 from sqlalchemy.dialects.postgresql import DOUBLE_PRECISION
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from query_funcs import query2
 import os
 
 Base = declarative_base()
@@ -101,3 +100,11 @@ player_file.close()
 query2(session, "Red")
 
 session.close()
+
+
+def query2(session, team_color):
+    team_names = session.query(Team).filter(
+        Color.name == team_color, Team.color_id == Color.color_id)
+    print("NAME")
+    for single_name in team_names:
+        print(single_name.name)
--- python/query_funcs.py
+++ python/query_funcs.py
@@ -5,15 +5,6 @@ from sqlalchemy.orm import sessionmaker
 import os
 
 
-def query2(session, team_color):
-    from main import Team, Color, State, Player
-    team_names = session.query(Team).filter(
-        Color.name == team_color, Team.color_id == Color.color_id)
-    print("NAME")
-    for single_name in team_names:
-        print(single_name.name)
-
-
 # engine = create_engine(
 #     "postgresql://postgres:passw0rd@localhost:5432/ACC_BBALL")
 # DBsession = sessionmaker(bind=engine)
