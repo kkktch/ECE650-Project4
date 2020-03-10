5d7a408e0b71a2e53a19dd0072e7b7e9d3db5133
kaidi <kl347@duke.edu>
2020-03-10 11:56:06 -0400

    test

--- python/main.py
+++ python/main.py
@@ -4,15 +4,23 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 
-class T1(Base):
-    __tablename__ = 't2'
+class State(Base):
+    __tablename__ = 'state'
 
     # emits SERIAL
-    id = Column(Integer, primary_key=True)
-    new_id = Column(Integer)
+    state_id = Column(Integer, primary_key=True)
+    name = Column(String(256))
+
+
+class Color(Base):
+    __tablename__ = 'color'
+
+    color_id = Column(Integer, primary_key=True)
+    name = Column(String(256))
 
 
 engine = create_engine(
     "postgresql://postgres:passw0rd@localhost:5432/ACC_BBALL", echo=True)
-T1.__table__.drop(engine, checkfirst=True)
+State.__table__.drop(engine, checkfirst=True)
+Color.__table__.drop(engine, checkfirst=True)
 Base.metadata.create_all(engine)
