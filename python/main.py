76d258b9413201a8fda79ec18b00f9d9bb944ea7
kaidi <kl347@duke.edu>
2020-03-10 15:00:40 -0400

    test

--- python/main.py
+++ python/main.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, Integer, String, Sequence
+from sqlalchemy import create_engine, Column, Integer, String, Sequence, DOUBLE_PRECISION
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -18,17 +18,37 @@ class Color(Base):
     name = Column(String(256))
 
 
-Re_Engine = create_engine(
-    "postgresql://postgres:passw0rd@localhost:5432/postgres")
-conn = Re_Engine.connect()
-conn.execute("commit")
-conn.execute("drop database if exists \"ACC_BBALL\"")
-conn.execute("commit")
-conn.execute("create database \"ACC_BBALL\"")
-conn.close()
+class Team(Base):
+    __tablename__ = 'team'
+
+    team_id = Column(Integer, autoincrement=True, primary_key=True)
+    name = Column(String(256))
+    state_id = Column(Integer, ForeignKey("state.state_id"))
+    color_id = Column(Integer, ForeignKey("color.color_id"))
+    wins = Column(Integer)
+    losses = Column(Integer)
+
+
+class Player(Base):
+    __tablename__ = 'player'
+
+    player_id = Column(Integer, autoincrement=True, primary_key=True)
+    team_id = Column(Integer, ForeignKey("team.team_id"))
+    uniform_num = Column(Integer)
+    first_name = Column(String(256))
+    last_name = Column(String(256))
+    mpg = Column(Integer)
+    ppg = Column(Integer)
+    rpg = Column(Integer)
+    apg = Column(Integer)
+    spg = Column(DOUBLE_PRECISION)
+    bpg = Column(DOUBLE_PRECISION)
+
 
 engine = create_engine(
     "postgresql://postgres:passw0rd@localhost:5432/ACC_BBALL", echo=True)
+Player.__table__.drop(engine, checkfirst=True)
+Team.__table__.drop(engine, checkfirst=True)
 State.__table__.drop(engine, checkfirst=True)
 Color.__table__.drop(engine, checkfirst=True)
 Base.metadata.create_all(engine)
