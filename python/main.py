73582165ae1514d7529bc69e5c44aad1f956f9d8
kaidi <kl347@duke.edu>
2020-03-10 11:37:35 -0400

    test

---
+++ python/main.py
@@ -0,0 +1,15 @@
+from sqlalchemy import create_engine, Column, Integer, String, Sequence
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class T2(Base):
+    __tablename__ = 't2'
+
+    # emits SERIAL
+    id = Column(Integer, primary_key=True)
+
+
+engine = create_engine("postgresql://postgres:passw0rd@localhost:5432/ACC_BBALL", echo=True)
+Base.metadata.create_all(engine)
