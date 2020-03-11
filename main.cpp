770dcd8e7c3b192e0e0b42b98ac0dd0d93b08389
kaidi <kl347@duke.edu>
2020-03-10 20:13:20 -0400

    test

--- main.cpp
+++ main.cpp
@@ -144,7 +144,6 @@ void initPlayer(connection *C)
   while (getline(read_file, content))
   {
     vector<string> contents = split(content);
-    ss >> id >> team_id >> uniform_num >> first_name >> last_name >> mpg >> ppg >> rpg >> apg >> spg >> bpg;
     add_player(C, contents[1], contents[2], contents[3], contents[4], contents[5], contents[6], contents[7], contents[8], contents[9], contents[10]);
   }
   read_file.close();
